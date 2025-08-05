6eab1b61c32f59bf6dcd5a522bdcd929ad2a3cf4
cdecugis <christophe.decugis@gmail.com>
2025-08-05 21:24:33 +0200

    2124

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,18 +20,6 @@ root.render(
 );
 
 
-///////////////// App Routes /////////////////
-export default function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/app" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
